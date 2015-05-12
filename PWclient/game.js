8e8e6e20c125c4bcc556fb299200c75e6148e870
Chris Cottle <chriscottle.wastaken@gmail.com>
2015-05-12 12:53:53 -0700

    Further refactoring

--- PWclient/game.js
+++ PWclient/game.js
@@ -5,16 +5,32 @@ function Game(windowWidth, windowHeight){
 	//PIXI init
 	this.renderer = PIXI.autoDetectRenderer(this.innerWidth,this.innerHeight);
 	this.stage = new PIXI.Stage(0xa5e0c9);
-	
-	//board needs dimensions of screen to determine render bounds
-	this.currentBoard = new Board(windowWidth,windowHeight);
+	this.stage.setInteractive = true;
 	
 	//rules vars
 	this.maxSelectedCards = 1;
 	this.numSelectedCards = 0;
 	this.cardMovementTimer = 1.0;
+	
+	//board needs dimensions of screen to determine render bounds
+	this.currentBoard = new Board(windowWidth,windowHeight);
+	
+	//push to window
+	document.body.appendChild(this.renderer.view);
 }
 
-function DoStep(){
+//define methods for each instance of a Game
+Game.prototype = {
+	constructor: Game, 
+	setMaxSelectedCards:function(given){
+		this.maxSelectedCards = given;
+	},
+	setNumSelectedCards:function(given){
+		this.numSelectedCards = given;
+	},
+	doStep:function(){
+		var current = new Card("UnknownCard.png");
+		this.stage.addChild(current.card);
+	}
 	
 }
--- PWclient/hand.js
+++ PWclient/hand.js
@@ -1,5 +1,7 @@
 //http://stackoverflow.com/questions/18582150/external-javascript-calling-functions-from-another-external-javascript-file
 function Hand(handSize, anchorX, anchorY){
-	
+	this.handSize = handSize;
+	this.handRotationFactor = 0.1;
+	this.cardHighlightBumpAmt = 20; //make this dynamic
 	
 }
